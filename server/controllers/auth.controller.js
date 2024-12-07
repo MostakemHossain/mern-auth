64e799511bf4203c702dd947f3a464331ca92a93
MostakemHossain <mostakememon123@gmail.com>
2024-12-07 17:17:03 +0600

    user logout function implement

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -89,3 +89,25 @@ export const login=async(req,res)=>{
         
     }
 }
+
+export const logout=async(req,res)=>{
+    try {
+        res.clearCookie('token',{
+            httpOnly:true,
+            secure:process.env.NODE_ENV ==="production",
+            sameSite: process.env.NODE_ENV ==="production" ? "none" : "strict",
+
+        })
+        return res.json({
+            status:true,
+            message:"logged out successfully"
+        })
+        
+    } catch (error) {
+        return res.json({
+            status: false,
+            message: error.message
+
+        })
+    }
+}
